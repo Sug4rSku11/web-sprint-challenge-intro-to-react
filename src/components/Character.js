675bedbb068dd6f91a28bd7f2ba48af97dc71226
desiree <desireegarcia@mini.local>
2021-10-08 12:53:54 -0600

    Completed styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,17 @@
 // Write your Character component here
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const StyledCharacter= styled.div`
 background-color: #d9c6a3;
 opacity:70%;
 display: flex;
-justify-content: center;
+justify-content: space-between;
 margin: 2%;
 .name {
     width: 30%;
+    text-align: left;
+    padding-left: 5%;
     &:hover {
     transition: all 0.2s ease-in-out;
     background-color: #f7ae28;
@@ -17,6 +19,8 @@ margin: 2%;
 }
 .birth {
     width: 30%;
+    text-align: left;
+    padding-left: 5%;
     &:hover {
     transition: all 0.2s ease-in-out;
     background-color: #b0dfff;
@@ -24,6 +28,8 @@ margin: 2%;
 }
 .gender {
     width:30%;
+    text-align: left;
+    padding-left: 10%;
     &:hover {
     transition: all 0.2s ease-in-out;
     background-color: #c4ffdc;
