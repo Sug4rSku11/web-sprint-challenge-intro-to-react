9ff31d11e4256abaac82e4d65eabb5671083fa09
desiree <desireegarcia@mini.local>
2021-10-08 12:39:25 -0600

    Styled div

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Star Wars </h1>
+      <h1 className="Header">Star Wars Characters</h1>
       {
         character.map((character) => 
         <Character character={character} />)
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,56 @@
 // Write your Character component here
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 
-
+const StyledCharacter= styled.div`
+background-color: #d9c6a3;
+opacity:70%;
+display: flex;
+justify-content: center;
+margin: 2%;
+.name {
+    width: 30%;
+    &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #f7ae28;
+  }
+}
+.birth {
+    width: 30%;
+    &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #b0dfff;
+  }
+}
+.gender {
+    width:30%;
+    &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #c4ffdc;
+  }
+}
+p {
+    font-weight: bold;
+    font-size: x-large;
+}
+`
 
 
 
 const Character = props=> {
     
     return (
-       
-            <div className='character-container'>
-             <p>{props.character.name}</p>
-             <p>{props.character.birth_year}</p>
+       <StyledCharacter>
+            <div className='name'>
+             <p>Name: {props.character.name}</p>
             </div>
+            <div className='birth'>            
+             <p>Birth Year: {props.character.birth_year}</p>
+            </div>
+            <div className='gender'>            
+             <p>Gender: {props.character.gender}</p>
+            </div>
+            </StyledCharacter>
         
     )
 }
