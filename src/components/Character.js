514494ca35f4eb9749f2f14c2b5b285cef480480
desiree <desireegarcia@mini.hsd1.nm.comcast.net>
2021-10-08 09:56:11 -0600

    started on App and Character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import App from '../App'
+
+
+
+const Characters = () => {
+
+
+return (
+    <div> Characters
+
+
+    </div>
+)
+
+}
+
+export default Characters;
